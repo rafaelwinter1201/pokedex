91375effc8d6ed761534c6648c769fc90f86b817
rafaelwinter1201 <rafael.winter.felipe@gmail.com>
2023-11-07 09:27:12 -0300

    aula92

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit {
+  public getAllPokemons:any;
 
+  constructor(
+    private pokeApiServices: PokeApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.pokeApiServices.apiListAllPokemons.subscribe(
+      res => {
+        this.getAllPokemons = res.results
+      }
+    );
+  }
 }
